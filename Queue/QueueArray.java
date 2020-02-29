7b5712e42f9461e37f3016c86e07ee7d739709ed
Collince-Okeyo <collinceokeyo98@gmail.com>
2020-02-29 18:50:15 +0300

    ChangeOnArray

--- Queue/QueueArray.java
+++ Queue/QueueArray.java
@@ -1,48 +1,40 @@
 public class QueueArray implements Queue {
-
     private Object arr[];
     private int f;
     private int r;
     public int N;
-
     public QueueArray(int capacity){
         arr = new Object[capacity];
         f = 0;
         r = 0;
         N = capacity;
     }
-
     @Override
     public int size() {
         return ((N-f+r)%N);
     }
-
     @Override
     public boolean isEmpty() {
         return (f==r);
     }
-
-     public boolean isFull(){
+  public boolean isFull(){
       return (r==N) 
     } 
-
     @Override
     public void enqueue(Object e) {
         if(isFull()){
             System.out.println("The queue is full");
         }
         else
-            arr[r] = e;
-            //r = ((r+1)%N);
-            r++;
+           arr[r] = e;
+           r = ((r+1)%N);
+           r++;
     }
-
     @Override
     public Object dequeue() {
         Object temp=null;
-
         if(isEmpty()){
-            System.out.println("The Queue is empty");
+            System.out.print("The Queue is empty");
         }
         else
             temp = arr[f];
@@ -50,15 +42,13 @@ public class QueueArray implements Queue {
             f = ((f+1)%N);
         return temp;
     }
-
     @Override
     public Object front() {
         return arr[f];
     }
-
     @Override
     public void display() {
-       for(int i = 0;i<arr.length;i++){
+       for(int i = 0;i<f;i++){
            System.out.print(arr[i]+" ");
        }
     }
